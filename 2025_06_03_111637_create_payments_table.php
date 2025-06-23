5fc8525024052071968e1e9316c998fb9e17bf7f
Wajeeha Nadeem <150377263+wajeehanadeem649@users.noreply.github.com>
2025-06-23 18:19:21 +0500

    Add files via upload

---
+++ 2025_06_03_111637_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('enrollment_id');
+            $table->date('paid_date');
+            $table->double('amount');
+            $table->foreign('enrollment_id')->references('id')->on('enrollments')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
